a2b42dd7605f1e40cabbd85ffaa8be59d26d5557
Cheryl Campbell <cherylcmpbll@yahoo.com>
2018-07-09 01:50:11 +0000

    Finished all Todos and ready to deploy

--- js/main.js
+++ js/main.js
@@ -7,35 +7,21 @@
 // Provide examples where it may help (e.g. show a comma-separated list of
 // toppings to indicate how to specify toppings, etc.)
 //
-// TODO: Prompt the user for what kind of bread they would like.
-// Ideally, that would look something like: "What kind of bread (white, wheat, flat)?"
-var bread = prompt("What kind of bread (white, wheat, or flat)?");
+// Customer order info
+let breadOrder = prompt("What kind of bread would you like (white, wheat, or flat)?", "wheat");
 
 
-// TODO: Prompt the user for what kind of meat(s) they would like.
-// Indicate they should separate multiple items womma if you would like more than one.)"
-var meat = prompt("What kind of meat? (ham, turkey, roast beef) Separate with commas if you would like more than one.");
-
-// 
-
-// TODO: Prompt the user for what kind of toppings they would like.
-// We expect this to be multiple, so ask them to provide you with a
-// comma-separated list using a user friendly prompt.
-var topping = prompt("Which toppings would you like? Please separate with commas. (lettuce, tomato, peppers, spinach)");
+let meatOrder = prompt("What kind of meat would you like? Please separate with commas if you would like more than one.");
 
 
-// TODO: Prompt the user for what kind of condiments they would like.
-// Again, we should expect a comma-separated list if items here.
-var condiment = prompt("What condiments would you like? Pleas separate with commas. (mayo, mustard, thousand island)");
+let toppingOrder = prompt("Which toppings would you like? Please separate with commas if multiple");
 
 
-// Step Two ////////////////////////////////////////////////////////////
-//
-// Process all the input to calculate values for the user's order. The named
-// values are initialized to `null`. Change that so they equal the proper value.
-//
-// The `prices` object below specifies the prices for each thing.
+let condimentOrder = prompt("Which condiments would you like? Please separate with commas.");
 
+
+
+// Process all the input to calculate values for the user's order.
 let prices = {
     sandwich: 5, // Base price for a sandwich is $5, includes bread
     meat: 1, // Each kind of meat on a sandwich costs $1
@@ -43,55 +29,48 @@ let prices = {
     condiment: 0.25 // Each condiment costs $0.25
 };
 
-// TODO: Convert order information from Strings to Arrays.
 
-let meatArray = ('ham', 'turkey', 'roast beef');
-let toppingArray = ('lettuce', 'tomato', 'peppers', 'spinach');
-let condimentArray = ('mayo', 'mustard', 'thousand island');
+let meatArray = meatOrder.split(',');
+let toppingArray = toppingOrder.split(',');
+let condimentArray = condimentOrder.split(',');
 
-// TODO: Calculate cost for meat, toppings, and condiments.
+// Calculate cost for meat, toppings, and condiments.
 // This requires you to determine the length of each Array you just made
 // and multiply out the costs. You will need to refer to the attributes of the
 // `prices` object in order to calculate these costs.
 
-let meatCost = null;
-let toppingCost = null;
-let condimentCost = null;
+let meatCost = meatArray.length * prices.meat;
+let toppingCost = toppingArray.length * prices.topping;
+let condimentCost = condimentArray.length * prices.condiment;
 
-// TODO: Combine the costs of each part of the sandwich to get the subtotal.
-let subtotal = null;
+// Combine the costs of each part of the sandwich to get the subtotal.
+let subtotal = prices.sandwich + meatCost + toppingCost + condimentCost;
 
-// TODO: Calculate the tax owed using the waStateTaxRate.
+// Calculate the tax owed using the waStateTaxRate.
 let waStateTaxRate = 0.065;
-let orderTax = null;
+let orderTax = subtotal * waStateTaxRate;
 
-// TODO: Calculate `totalPrice` by adding `subtotal` and `orderTax`.
-let totalPrice = null;
+// Calculate `totalPrice` by adding `subtotal` and `orderTax`.
+let totalPrice = subtotal + orderTax;
 
 
-// Step Three //////////////////////////////////////////////////////////
-//
-// TODO: Now that we've calculated all the values, insert them into this
-// template literal using proper expression tags. Note that we must provide
-// all of the info the user gave us to confirm the order, and then we must also
-// provide the cost information so the user understands their bill.
 
 let receiptTemplate = `
     <p>SANDWICH ORDER</p>
-    <p>Bread: wheat</p>
-    <p>Meat: ham, turkey</p>
-    <p>Toppings: lettuce, tomato, peppers, spinach</p>
-    <p>Condiments: mayo, mustard, thousand island</p>
+    <p>Bread: ${breadOrder}</p>
+    <p>Meat: ${meatOrder}</p>
+    <p>Toppings: ${toppingOrder}</p>
+    <p>Condiments: ${condimentOrder}</p>
     <p>---------------------</p>
-    <p class="text-right">Sandwich: $4.42</p>
-    <p class="text-right">Meat: $2.00</p>
-    <p class="text-right">Toppings: $2.00</p>
-    <p class="text-right">Condiments: $1.42</p>
+    <p class="text-right">Sandwich: $${prices.sandwich.toFixed(2)}</p>
+    <p class="text-right">Meat: $${meatCost.toFixed(2)}</p>
+    <p class="text-right">Toppings: $${toppingCost.toFixed(2)}</p>
+    <p class="text-right">Condiments: $${condimentCost.toFixed(2)}</p>
     <p class="text-right">--------</p>
-    <p class="text-right">Subtotal: $9.84</p>
-    <p class="text-right">Tax: $1.42</p>
+    <p class="text-right">Subtotal: $${subtotal.toFixed(2)}</p>
+    <p class="text-right">Tax: $${orderTax.toFixed(2)}</p>
     <p class="text-right">--------</p>
-    <p class="text-right">Total: $4.84</p>
+    <p class="text-right">Total: $${totalPrice.toFixed(2)}</p>
 `
 
 ///////////////////////////////////////////////////////////////////////
